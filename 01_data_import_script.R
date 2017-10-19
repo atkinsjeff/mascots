41c661ad944ed0485599bbaffeb1e9a2a5471b2e
Jeff <jwatkins6@vcu.edu>
2017-10-19 14:25:17 -0400

    first commit

---
+++ 01_data_import_script.R
@@ -0,0 +1,6 @@
+# Jeff Atkins October, 2017 (@atkinsjeff)
+#
+# This script imports a data set of professional sports teams with mascots and associated information in an attempt to look at spatial patterns in mascot selection
+# 
+# 
+
