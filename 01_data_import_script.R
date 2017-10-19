d5cef43af52cc6810bbc3393f700348bdc14a6f9
Jeff <jwatkins6@vcu.edu>
2017-10-19 14:50:36 -0400

    reclassifying pirate as an occupation

--- 01_data_import_script.R
+++ 01_data_import_script.R
@@ -3,4 +3,18 @@
 # This script imports a data set of professional sports teams with mascots and associated information in an attempt to look at spatial patterns in mascot selection
 # 
 # 
+require(ggplot2)
+require(plyr)
+require(dplyr)
+require(tidyr)
+require(magrittr)
+
+# Importing data
+df <- read.csv("./data/mascots.csv")
+
+# Let's look at the class variable
+table(df$class)
+
+dim(df)
+
 
